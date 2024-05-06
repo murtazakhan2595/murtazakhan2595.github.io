18e2c74acc5043fadf8bb437caf8f42d7491ff0e
ahmedccript <murtazakhan2595@gmail.com>
2024-05-06 18:00:42 +0500

    tasks updated

--- lab10/index.php
+++ lab10/index.php
@@ -3,16 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+    <title>PHP Tasks</title>
 </head>
 <body>
-    <h2>Login</h2>
-    <form action="login.php" method="post">
-        <label for="username">Username:</label><br>
-        <input type="text" id="username" name="username"><br>
-        <label for="password">Password:</label><br>
-        <input type="password" id="password" name="password"><br><br>
-        <input type="submit" value="Login">
-    </form>
+    <h2>PHP Tasks</h2>
+    <ul>
+        <li><a href="login/index.php">Task 1: Login Form</a></li>
+        <li><a href="dec-bin/index.php">Task 2: Decimal to Binary Converter</a></li>
+        <li><a href="class-std/index.php">Task 3: Student class data</a></li>
+        <li><a href="city-print/index.php">Task 4: Favourate city</a></li>
+    </ul>
 </body>
 </html>
